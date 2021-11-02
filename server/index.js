a400700295acc6000382a81925d763b6d00efb05
suthyscott <sutherlandscott21@gmail.com>
2021-11-02 15:34:01 -0600

    app endpoint

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const cors = require('cors')
-
+const path = require('path')
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
 const motorcycleCtrl = require('./motorcycleCtrl')
+
+app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+  });
 
 app.post('/motorcycle', motorcycleCtrl.addMotorcycle)
 
